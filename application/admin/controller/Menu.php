9c53740d43ca6bbcbbb2df4b449b32cabd4fffb9
nie <yesheng35@126.com>
2022-02-07 13:47:16 +0800

    123

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -1,60 +1,84 @@
 <?php
+
 namespace app\admin\controller;
 
 use think\Loader;
+use think\Log;
 
 class Menu extends Base
 {
-	private $model;
-	private $logic;
-	function _initialize(){
-		parent::_initialize();
-		$this->model=Loader::model("AuthMenu");
-		$this->logic=Loader::model('AuthMenu','logic');
-	}
-	public function index(){
-		if($this->page!=null){
-			//$this->logic->getPageWithAdmin($this->page,$this->limit);exit;
-            $map = ['pid'=>0];
-            $Menu = $this->model->where($map)->select();
+    private $model;
+    private $logic;
+
+    function _initialize()
+    {
+        parent::_initialize();
+        $this->model = Loader::model("AuthMenu");
+        $this->logic = Loader::model('AuthMenu', 'logic');
+    }
+
+    public function index()
+    {
+        if (Request()->isPost()) {
+            //$this->logic->getPageWithAdmin($this->page,$this->limit);exit;
+            $map   = ['pid' => 0];
+            $Menu  = $this->model->where($map)->select();
             $count = $this->model->where($map)->count();
-            $data = [
+            $data  = [
                 'code' => 0,
-                'msg' => '',
+                'msg'  => '',
                 'data' => [
                     'count' => $count,
-                    'list' => $Menu
+                    'list'  => $Menu
                 ],
             ];
             return json($data);
-		}
-		return view();
-	}
-	function add(){
-		if(request()->isPost()){
-			$this->logic->add_one(input('post.'));
-		}
-		$menuList=$this->logic->get_all_menu(['pid'=>0]);
-		return view('', [
-			'menuList'=>$menuList
-		]);
-	}
-	function edit($id=""){
-		if(request()->isPost()){
-			$this->logic->save_one(input('post.'));
-		}
-		$info = $this->logic->get_find($id);
-		$menuList = $this->logic->get_all_menu(['pid'=>0]);
-		return view('', [
-			'info'=>$info,
-			'menuList'=>$menuList
-		]);
-	}
-	
-	public function delete($id = "")
-	{
-		if ($id != "") {
-			$this->logic->delete($id);
-		}
-	}
+        }
+        return view();
+    }
+
+    function add()
+    {
+        if (request()->isPost()) {
+            $this->logic->add_one(input('post.'));
+        }
+        $menuList = $this->logic->get_all_menu(['pid' => 0]);
+        return view('', [
+            'menuList' => $menuList
+        ]);
+    }
+
+    function edit($id = "")
+    {
+        if (request()->isPost()) {
+            $data = input('post.');
+            $id = request()->get('id');
+            $state = model('AuthMenu')->update($data, ["id"=>$id]);
+            if($state !== false ) {
+                return json([
+                    'msg' => '成功',
+                    'data' => [],
+                    'code' => 200,
+                ], 200);
+            }
+            return json([
+                'msg' => '失败',
+                'data' => [],
+                'code' => 400,
+            ], 200);
+        }
+        $info     = $this->logic->get_find($id);
+        $menuList = $this->logic->get_all_menu(['pid' => 0]);
+        return view('', [
+            'info'     => $info,
+            'menuList' => $menuList
+        ]);
+    }
+
+    public function delete($id = "")
+    {
+        if ($id != "") {
+            $this->logic->delete($id);
+        }
+    }
 }
--- application/admin/model/AuthMenu.php
+++ application/admin/model/AuthMenu.php
@@ -1,7 +1,15 @@
 <?php
 namespace app\admin\model;
  
+use think\Log;
 use think\Model;
  
 class AuthMenu extends Model{
+
+    protected static function init()
+    {
+       AuthMenu::beforeUpdate(function($data){
+           Log::log('AuthMenuAuthMenuAuthMenuAuthMenuAuthMenuAuthMenu');
+       });
+    }
 }
