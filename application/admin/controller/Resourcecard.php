27dbe366d3bd0321a3458ea13732e8427a3b8f5c
Lucius <yesheng35@126.com>
2022-02-10 19:20:05 +0800

    1234

--- application/admin/controller/Resourcecard.php
+++ application/admin/controller/Resourcecard.php
@@ -21,6 +21,22 @@ class Resourcecard extends Controller
             $page = request()->param('page');
             $limit = request()->param('limit');
             $offset = ($page - 1) * $limit;
+            $resources_id = request()->param('resources_id');
+            $to_uid = request()->param('to_uid');
+            $from_uid = request()->param('from_uid');
+            $read_status = request()->param('read_status');
+            if(!empty($resources_id)) {
+                $map['resources_id'] = $resources_id;
+            }
+            if(!empty($to_uid)) {
+                $map['to_uid'] = $to_uid;
+            }
+            if(!empty($from_uid)) {
+                $map['from_uid'] = $from_uid;
+            }
+            if(!empty($read_status)) {
+                $map['read_status'] = $read_status;
+            }
             $ResourceCard = model('ResourceCard');
             $data = $ResourceCard->where($map)->limit($offset, $limit)->select();
             $count = $ResourceCard->where($map)->count();
