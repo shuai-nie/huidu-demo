ec4cb3d12956aecf53b76aedf1086af7e30a17dc
Lucius <yesheng35@126.com>
2022-02-11 19:07:46 +0800

    12341234

--- application/admin/controller/Card.php
+++ application/admin/controller/Card.php
@@ -15,17 +15,12 @@ class Card extends Controller
     public function index()
     {
         if(Request()->isPost()) {
-            $map = ['A.status'=>1];
+            $map = ['status'=>1];
             $page = Request()->param('page');
             $limit = Request()->param('limit');
             $offset = ($page - 1) * $limit;
-            $CardAll = model("Card")->alias('A')
-                ->join(model('CardContact')->getTable().' B', 'A.id=B.card_id', 'LEFT')
-                ->field('A.*,B.contact_type,B.contact_number')
-                ->where($map)->limit($offset, $limit)->select();
-            $count = model("Card")->alias('A')
-                ->join(model('CardContact')->getTable().' B', 'A.id=B.card_id', 'LEFT')
-                ->where($map)->count();
+            $CardAll = model("Card")->where($map)->limit($offset, $limit)->select();
+            $count = model("Card")->where($map)->count();
             return json(['data'=>['count'=>$count, 'list'=>$CardAll]], 200);
         }
         return view('');
@@ -40,37 +35,13 @@ class Card extends Controller
     {
         if(Request()->isPost()) {
             $data = Request()->param();
-            $data['create_id'] = getLoginUserId();
-            $data['update_id'] = getLoginUserId();
-            $state = model("Card")->save($data);
+            $state = true;
             if($state !== false){
                 return success_json();
             }
             return error_json();
         }
         return view();
-    }
-
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
     }
 
     /**
@@ -81,7 +52,33 @@ class Card extends Controller
      */
     public function edit($id)
     {
-        //
+        if(\request()->isPost()) {
+            $_post = \request()->param();
+            $contact = [];
+            foreach ($_post['contact'] as $k=>$v) {
+                array_push($contact, [
+                    'card_id' => $id,
+                    'contact_type' => $v,
+                    'contact_number' => $_post['tel'][$k]
+                ]);
+            }
+            $state = model('Card')->save($_post, ['id'=>$id]);
+            model('CardContact')->where(['card_id'=>$id])->delete();
+            model('CardContact')->saveAll($contact);
+            if($state !== false) {
+                return success_json(lang('EditSuccess', [lang('CARD')] ));
+            }
+            return error_json(lang('EditSuccess', [lang('CARD')]) );
+
+        }
+        $data = model('Card')->find($id);
+        $DataDicData = model('DataDic')->where(['data_type_no'=>'CONTACT_TYPE','status'=>1])->order('sort desc')->select();
+        $CardContact = model('CardContact')->where(['card_id'=>$data['id']])->select();
+        return view('', [
+            'data'=>$data,
+            'DataDicData' => $DataDicData,
+            'CardContact' => $CardContact,
+        ]);
     }
 
     /**
