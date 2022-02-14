0dd2d65a7d28a46da97ef76b56ce334eb1a9816a
Lucius <yesheng35@126.com>
2022-02-14 17:58:58 +0800

    1234

--- application/admin/controller/Remark.php
+++ application/admin/controller/Remark.php
@@ -10,6 +10,16 @@ class Remark extends Base{
     public function index()
     {
         $list = model('AdminLog')->order('id desc')->limit(10)->select();
-        return view('', ['list' => $list]);
+        $CounselorCount = model('Counselor')->where(['status'=>1])->count();
+        $ResourceCount = model('Resource')->where(['status'=>1])->count();
+        $AdvertisementCount = model('Advertisement')->where(['status'=>1])->count();
+        $UserCount = model('User')->where(['status'=>1])->count();
+        return view('', [
+            'list' => $list,
+            'CounselorCount' => $CounselorCount,
+            'ResourceCount' => $ResourceCount,
+            'AdvertisementCount' => $AdvertisementCount,
+            'UserCount' => $UserCount,
+        ]);
     }
 }
