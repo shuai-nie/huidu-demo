319ff53202df7e1e1b53c8fe94cbc2f85dbb53d1
Lucius <yesheng35@126.com>
2022-03-16 15:05:27 +0800

    123123

--- application/admin/model/Package.php
+++ application/admin/model/Package.php
@@ -6,5 +6,7 @@ use think\Model;
 
 class Package extends Base
 {
-    //
+    protected $autoWriteTimestamp = true;
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
 }
