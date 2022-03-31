50da552b07f225b2d415b4b34d02eaf8a1f1ea82
Lucius <yesheng35@126.com>
2022-03-31 17:46:00 +0800

    资源·表单模板

--- application/admin/controller/ResourceFormTemplate.php
+++ application/admin/controller/ResourceFormTemplate.php
@@ -26,17 +26,49 @@ class ResourceFormTemplate extends Controller
     public function index()
     {
         $ResourceFormTemplate = model('ResourceFormTemplate');
+        $DataDic = model('DataDic');
         if(\request()->isPost()){
-            $map = array();
+            $map = array('A.status'=>1);
             $limit = \request()->post('limit');
             $page = \request()->post('page');
+            $fill_flag = \request()->post('fill_flag');
+            $form_type = \request()->post('form_type');
+            $ty = \request()->post('ty');
+            $business_subdivide = \request()->post('business_subdivide');
+            $form_title = \request()->post('form_title');
             $offset = ($page - 1) * $limit;
-            $data = $ResourceFormTemplate->alias('A')->where($map)->limit($offset, $limit)->select();
-            $count = $ResourceFormTemplate->alias('A')->where($map)->count();
+            if(is_numeric($fill_flag)){
+                $map['A.fill_flag'] = $fill_flag;
+            }
+            if(is_numeric($form_type)){
+                $map['A.form_type'] = $form_type;
+            }
+            if(is_numeric($ty)){
+                $map['A.ty'] = $ty;
+            }
+            if(is_numeric($business_subdivide)){
+                $map['A.business_subdivide'] = $business_subdivide;
+            }
+            if(!empty($form_title)){
+                $map['A.form_title'] = ['like', "%{$form_title}%"];
+            }
+            $data = $ResourceFormTemplate->alias('A')
+                ->join($DataDic->getTable().' B', '(A.type=B.data_no and B.data_type_no="RESOURCES_TYPE")', 'left')
+                ->join($DataDic->getTable().' C', '(A.business_subdivide=C.data_no and C.data_type_no="RESOURCES_SUBDIVIDE")', 'left')
+                ->field('A.*,B.data_type_name,B.data_name AS type_name,C.data_name AS business_subdivide_name')
+                ->where($map)->limit($offset, $limit)->order('id desc')->select();
+            $count = $ResourceFormTemplate->alias('A')
+                ->join($DataDic->getTable().' B', '(A.type=B.data_no and B.data_type_no="RESOURCES_TYPE")', 'left')
+                ->join($DataDic->getTable().' C', '(A.business_subdivide=C.data_no and C.data_type_no="RESOURCES_SUBDIVIDE")', 'left')
+                ->where($map)->count();
             return json(['data' => ['count' => $count, 'list' => $data]], 200);
         }
+        $resourcesSubdivide = $DataDic->where(['data_type_no'=>'RESOURCES_SUBDIVIDE', 'status'=>1])->select();
         return view('', [
-            'meta_title' => '菜单·资源类型',
+            'meta_title' => '资源·表单模板',
+            'form_type' => $this->form_type,
+            'ty' => (new Resource())->ty,
+            'resourcesSubdivide' => $resourcesSubdivide,
         ]);
     }
 
@@ -48,8 +80,11 @@ class ResourceFormTemplate extends Controller
     public function create()
     {
         $ResourceFormTemplate = model('ResourceFormTemplate');
+        $DataDic = model('DataDic');
         if(\request()->isPost()){
             $_post = \request()->post();
+            $DataDicInfo = $DataDic->where(['data_type_no'=>'RESOURCES_SUBDIVIDE','status'=>1, 'data_no'=>$_post['business_subdivide']])->find();
+            $_post['type'] = $DataDicInfo['data_top_id'];
             $state = $ResourceFormTemplate->save($_post);
             if($state !== false) {
                 return success_json(lang('CreateSuccess', [lang('ResourceFormTemplate')]));
@@ -57,9 +92,11 @@ class ResourceFormTemplate extends Controller
             return error_json(lang('CreateFail', [lang('ResourceFormTemplate')]));
         }
         $ty = (new Resource())->ty;
+        $resourcesSubdivide = $DataDic->where(['data_type_no'=>'RESOURCES_SUBDIVIDE', 'status'=>1])->select();
         return view('', [
             'ty' => $ty,
             'form_type' => $this->form_type,
+            'resourcesSubdivide' => $resourcesSubdivide,
         ]);
     }
 
@@ -72,8 +109,11 @@ class ResourceFormTemplate extends Controller
     public function edit($id)
     {
         $ResourceFormTemplate = model('ResourceFormTemplate');
+        $DataDic = model('DataDic');
         if(\request()->isPost()){
             $_post = \request()->post();
+            $DataDicInfo = $DataDic->where(['data_type_no'=>'RESOURCES_SUBDIVIDE','status'=>1, 'data_no'=>$_post['business_subdivide']])->find();
+            $_post['type'] = $DataDicInfo['data_top_id'];
             $state = $ResourceFormTemplate->save($_post, ['id' => $id]);
             if($state !== false) {
                 return success_json(lang('EditSuccess', [lang('ResourceFormTemplate')]));
@@ -82,10 +122,12 @@ class ResourceFormTemplate extends Controller
         }
         $ty = (new Resource())->ty;
         $ResourceFormTemplateInfo = $ResourceFormTemplate->find($id);
+        $resourcesSubdivide = $DataDic->where(['data_type_no'=>'RESOURCES_SUBDIVIDE', 'status'=>1])->select();
         return view('', [
             'ty' => $ty,
             'form_type' => $this->form_type,
             'ResourceFormTemplateInfo' => $ResourceFormTemplateInfo,
+            'resourcesSubdivide' => $resourcesSubdivide,
         ]);
 
     }
