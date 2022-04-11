02c215d61825723becdabb8ee6ebf6c85780f010
Lucius <yesheng35@126.com>
2022-04-11 10:24:11 +0800

    资源投诉处理

--- application/admin/controller/Complaint.php
+++ application/admin/controller/Complaint.php
@@ -13,13 +13,26 @@ class Complaint extends Base
      */
     public function index()
     {
+
         if(\request()->isPost()) {
             $page = \request()->post('page');
             $limit = \request()->post('limit');
             $Complaint = model('Complaint');
+            $DataDic = model('DataDic');
             $offset = ($page - 1 ) * $limit;
             $map = [] ;
             $data = $Complaint->where($map)->limit($offset, $limit)->order('id desc')->select();
+            foreach ($data as $k => $v){
+                $detail_cause = json_decode($v['detail_cause'], true);
+                $detail_cause = implode(',', $detail_cause);
+                $v['detail_cause'] = $detail_cause;
+
+                $DataDicFind = $DataDic->where(['status'=>1,'data_type_no'=>'REPORT_TYPE','data_no'=>$v['report_type']])->field('data_no,data_name')->find();
+                $v['report_type'] = $DataDicFind['data_name'];
+                $UserFind = CacheUser($v['uid']);
+                $v['username'] = $UserFind['username'];
+                $data[$k] = $v;
+            }
             $count = $Complaint->where($map)->count();
             return json(['data'=>['count'=>$count, 'list'=>$data]], 200);
         }
@@ -27,25 +40,30 @@ class Complaint extends Base
     }
 
     /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
      * 显示编辑资源表单页.
-     *
      * @param  int  $id
      * @return \think\Response
      */
     public function edit($id)
     {
-        //
+        $Complaint = model('Complaint');
+        if(\request()->isPost()){
+            $disposeResult = \request()->post('dispose_result');
+            $state = $Complaint->save(['dispose_result' => $disposeResult], ['id' => $id]);
+            if($state !== false){
+                return success_json('提交成功');
+            }
+            return error_json('提交失败');
+        }
+        $data = $Complaint->find($id);
+        if($data['detail_cause']){
+            $detail_cause = json_decode($data['detail_cause'], true);
+            $detail_cause = implode(',', $detail_cause);
+            $data['detail_cause'] = $detail_cause;
+        }
+        $userAll = model('User')->where(['status'=>1])->field('id,username')->select();
+        $DataDicAll = model('DataDic')->where(['status'=>1,'data_type_no'=>'REPORT_TYPE'])->field('data_no,data_name')->select();
+        return view('', ['data' => $data, 'user' => $userAll, 'DataDicAll' => $DataDicAll]);
     }
 
     /**
