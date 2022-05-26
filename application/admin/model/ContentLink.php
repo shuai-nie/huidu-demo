602a4e87d638efdb9450601f5c29bda5be965e2b
Lucius <yesheng35@126.com>
2022-05-26 15:01:09 +0800

    更新关键词列表

---
+++ application/admin/model/ContentLink.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\admin\model;
+
+
+class ContentLink extends Base
+{
+    protected $connection = [
+        'prefix' => 'jm_',
+    ];
+
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
+    protected $insert = ['status' => 1, 'create_id', 'update_id'];
+    protected $update = ['update_id'];
+
+    protected function setCreateIdAttr()
+    {
+        return getLoginUserId();
+    }
+
+    protected function setUpdateIdAttr()
+    {
+        return getLoginUserId();
+    }
+}
