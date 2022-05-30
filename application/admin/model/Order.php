76ac99a8c0a7e973b9ed2121706ad7e19e5cfaa5
Lucius <yesheng35@126.com>
2022-05-30 14:42:34 +0800

    更新新的功能

---
+++ application/admin/model/Order.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\admin\model;
+
+class Order extends Base
+{
+    protected $autoWriteTimestamp = true;
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
+    protected $insert = ['status' => 1, 'create_id', 'update_id'];
+    protected $update = ['update_id'];
+
+    protected function setCreateIdAttr()
+    {
+        return getLoginUserId();
+    }
+
+    protected function setUpdateIdAttr()
+    {
+        return getLoginUserId();
+    }
+}
