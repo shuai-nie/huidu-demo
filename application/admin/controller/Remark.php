8950557d76c7a652e3a63a57990871991395a9fe
Lucius <yesheng35@126.com>
2022-06-10 17:38:25 +0800

    更新

--- application/admin/controller/Remark.php
+++ application/admin/controller/Remark.php
@@ -28,74 +28,35 @@ class Remark extends Base{
         ]);
     }
 
-    public function statistics()
-    {
-        $data = [
-            ['id'=>1,'pid'=>0,'data_name'=>'资源·合作领域','isedit'=>false],
-            ['id'=>2,'pid'=>0,'data_name'=>'联系方式','isedit'=>false],
-            ['id'=>3,'pid'=>0,'data_name'=>'资源·货币','isedit'=>false],
-            ['id'=>4,'pid'=>0,'data_name'=>'举报类型','isedit'=>false],
-            ['id'=>5,'pid'=>0,'data_name'=>'资源·合作区域','isedit'=>false],
-        ];
-        $DataDic = model('DataDic');
-
-        // 业务类型
-        $bb = $this->DataDicAllUp();
-        dump($bb);exit();
-        return $data;
-
-        /*$arrType = $DataDic->where(['status'=>1, 'data_type_no'=>'CONTACT_TYPE'])->select();
-        foreach ($arrType as $k1 => $v1) {
-            array_push($data, ['id' =>$v1['id'] ,'pid'=>2, 'title'=>$v1['data_name'],'data_no'=>$v1['data_no'], 'data_name'=>$v1['data_name'], 'data_icon'=>$v1['data_icon'], 'data_dark_icon'=>$v1['data_dark_icon'], 'sort'=>$v1['sort'], 'url_keyword'=>$v1['url_keyword']]);
-        }
-
-        $arrCur = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCE_CURRENCY'])->select();
-        foreach ($arrCur as $k1 => $v1) {
-            array_push($data, ['id' =>$v1['id'] ,'pid'=>3, 'title'=>$v1['data_name'],'data_no'=>$v1['data_no'], 'data_name'=>$v1['data_name'], 'data_icon'=>$v1['data_icon'], 'data_dark_icon'=>$v1['data_dark_icon'], 'sort'=>$v1['sort'], 'url_keyword'=>$v1['url_keyword']]);
-        }
-        $dataAll = $DataDic->where(['status'=>1, 'data_type_no'=>'REPORT_TYPE'])->select();
-        foreach ($dataAll as $k => $v) {
-            array_push($data, ['id' =>$v['id'] ,'pid'=>4, 'title'=>$v['data_name'], 'data_no'=>$v['data_no'], 'data_name'=>$v['data_name'], 'data_icon'=>$v['data_icon'], 'data_dark_icon'=>$v['data_dark_icon'], 'sort'=>$v['sort'], 'url_keyword'=>$v['url_keyword']]);
-            $all = $DataDic->where(['status'=>1, 'data_type_no'=>'REPORT_DETAIL_CAUSE', 'data_top_id'=>$v['data_no']])->select();
-            foreach ($all as $k1 => $v1) {
-                array_push($data, ['id' =>$v1['id'] ,'pid'=>$v['id'], 'title'=>$v1['data_name'],'data_no'=>$v1['data_no'], 'data_name'=>$v1['data_name'], 'data_icon'=>$v1['data_icon'], 'data_dark_icon'=>$v1['data_dark_icon'], 'sort'=>$v1['sort'], 'url_keyword'=>$v1['url_keyword']]);
-            }
-        }
-
-        $resourcesAll = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_REGION'])->select();
-        foreach ($resourcesAll as $k => $v) {
-            array_push($data, ['id' =>$v['id'] ,'pid'=>5, 'title'=>$v['data_name'], 'data_no'=>$v['data_no'], 'data_name'=>$v['data_name'], 'data_icon'=>$v['data_icon'], 'data_dark_icon'=>$v['data_dark_icon'], 'sort'=>$v['sort'], 'url_keyword'=>$v['url_keyword']]);
-        }*/
-
-    }
-
     protected function DataDicAllUp()
     {
         if($data = Cache::get('name')){
             return $data;
         } else {
+            $Resource = model('Resource');
             $DataDic = model('DataDic');
-
             $field = "id,data_type_no,data_type_name,data_top_id,data_no,data_name";
             $arrType = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_TYPE'])->field($field)->order('sort desc')->select();
-            $arrTypeCountZ = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_TYPE'])->count();
             foreach ($arrType as $k1 => $v1) {
-                $arrTypeCount = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_SUBDIVIDE','data_top_id'=>$v1['data_no']])->count();
                 $v1['level'] = 1;
-                $v1['count'] = $arrTypeCount;
+                $v1['data_type_name'] = '合作领域';
+                $v1['count'] = $Resource->selectCount(['type' => $v1['data_no']]);
                 $arrSub = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_SUBDIVIDE','data_top_id'=>$v1['data_no']])->field($field)->order('sort desc')->select();
                 foreach ($arrSub as $k2 => $v2) {
-                    $count = $DataDic->where(['status' => 1, 'data_type_no' => 'RESOURCE_INDUSTRY', 'data_top_id' => $v2['id']])->count();
                     $v2['level'] = 2;
-                    $v2['count'] = $count;
+                    $v2['count'] = $Resource->selectCount(['business_subdivide' => $v2['data_no']]);;
+                    $v1['data_type_name'] = '业务细分';
                     $arrInd = $DataDic->where(['status' => 1, 'data_type_no' => 'RESOURCE_INDUSTRY', 'data_top_id' => $v2['id']])->field($field)->order('sort desc')->select();
                     foreach ($arrInd as $k3 => $v3) {
-                        $count = $DataDic->where(['status' => 1, 'data_type_no' => 'RESOURCE_INDUSTRY_SUBDIVIDE', 'data_top_id' => $v3['data_no']])->count();
                         $v3['level'] = 3;
-                        $v3['count'] = $count;
+                        $v1['data_type_name'] = '对应行业';
+                        $v3['count'] = $Resource->selectCount(['industry'=>$v3['data_no']]);
                         $arrIndSub = $DataDic->where(['status' => 1, 'data_type_no' => 'RESOURCE_INDUSTRY_SUBDIVIDE', 'data_top_id' => $v3['data_no']])->field($field)->order('sort desc')->select();
                         foreach ($arrIndSub as $k4 => $v4) {
                             $v4['level'] = 4;
+                            $v4['data_type_name'] = '行业细分';
+                            $count = $Resource->selectSubdivideCount($v4['data_no']);
+                            $v4['count'] = $count[0]['count_as'];
                             $arrIndSub[$k4] = $v4;
                         }
                         $v3['child'] = $arrIndSub;
@@ -108,39 +69,11 @@ class Remark extends Base{
                 $arrType[$k1] = $v1;
             }
 
-            $dataTypeNo = $DataDic->where(['status'=>1, 'data_type_no'=>'CONTACT_TYPE'])->field($field)->select();
-            $dataTypeNoCount = $DataDic->where(['status'=>1, 'data_type_no'=>'CONTACT_TYPE'])->count();
-            $dataCurrency = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCE_CURRENCY'])->field($field)->select();
-            $dataCurrencyCount = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCE_CURRENCY'])->count();
-            $dataType = $DataDic->where(['status'=>1, 'data_type_no'=>'REPORT_TYPE'])->field($field)->select();
-            $dataTypeCount = $DataDic->where(['status'=>1, 'data_type_no'=>'REPORT_TYPE'])->count();
             $dataRegion = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_REGION'])->field($field)->select();
-            $dataRegionCount = $DataDic->where(['status'=>1, 'data_type_no'=>'RESOURCES_REGION'])->count();
-
-            foreach ($dataTypeNo as $kNo => $valNo){
-                $valNo['level'] = 4;
-                $valNo['count'] = 0;
-                $valNo['child'] = array();
-                $dataTypeNo[$kNo] = $valNo;
-            }
-
-            foreach ($dataCurrency as $kNo => $valNo){
-                $valNo['level'] = 4;
-                $valNo['count'] = 0;
-                $valNo['child'] = array();
-                $dataCurrency[$kNo] = $valNo;
-            }
-
-            foreach ($dataType as $kNo => $valNo){
-                $valNo['level'] = 4;
-                $valNo['count'] = 0;
-                $valNo['child'] = array();
-                $dataType[$kNo] = $valNo;
-            }
-
             foreach ($dataRegion as $kNo => $valNo){
+                $count = $Resource->selectRegionCount($valNo['data_no']);
                 $valNo['level'] = 4;
-                $valNo['count'] = 0;
+                $valNo['count'] = $count[0]['count'];
                 $valNo['child'] = array();
                 $dataRegion[$kNo] = $valNo;
             }
@@ -150,41 +83,19 @@ class Remark extends Base{
                     'data_type_name' => '资源*合作领域',
                     'data_name' => '',
                     'level' => 0,
-                    'count' => $arrTypeCountZ,
+                    'count' => 0,
                     'child' => $arrType
-                ),
-                array(
-                    'data_type_name' => '联系方式',
-                    'data_name' => '',
-                    'level' => 0,
-                    'count' => $dataTypeNoCount,
-                    'child' => $dataTypeNo
-                ),
-                array(
-                    'data_type_name' => '资源货币',
-                    'data_name' => '',
-                    'level' => 0,
-                    'count' => $dataCurrencyCount,
-                    'child' => $dataCurrency
-                ),
-                array(
-                    'data_type_name' => '举报类型',
-                    'data_name' => '',
-                    'level' => 0,
-                    'count' => $dataTypeCount,
-                    'child' => $dataType
                 ),
                 array(
                     'data_type_name' => '资源*合作区域',
                     'data_name' => '',
                     'level' => 0,
-                    'count' => $dataRegionCount,
+                    'count' => 0,
                     'child' => $dataRegion
                 ),
             );
             Cache::set('dataStatic', $data, 3600);
             return $data;
-
         }
     }
 
--- application/admin/model/Resource.php
+++ application/admin/model/Resource.php
@@ -2,6 +2,7 @@
 
 namespace app\admin\model;
 
+use think\Db;
 use think\Model;
 
 class Resource extends Base
@@ -9,4 +10,29 @@ class Resource extends Base
     protected $autoWriteTimestamp = true;
     protected $createTime = 'create_time';
     protected $updateTime = 'update_time';
+
+    public function selectCount($map)
+    {
+        $map['status'] = 1;
+        $map['auth'] = 1;
+        $data=  $this->where($map)->count();
+        //echo $this->getLastSql() . "\n\t";
+        return $data;
+    }
+
+    // 合作区域
+    public function selectRegionCount($id)
+    {
+        // SELECT * from (select id,REPLACE(region, '|', ',') as region FROM hc_resource  ) as t  where FIND_IN_SET(12, region )
+        //return $this->where($map)->count();
+        return Db::query("SELECT count(*) AS count from (select id,status,auth,REPLACE(region, '|', ',') as region FROM hc_resource  ) as t where status=1 and auth = 1 and  FIND_IN_SET(?, region )", [$id]);
+    }
+
+    // 行业细分
+    public function selectSubdivideCount($id)
+    {
+        // SELECT * from (select id,REPLACE(region, '|', ',') as region FROM hc_resource  ) as t  where FIND_IN_SET(12, region )
+        //return $this->where($map)->count();
+        return Db::query("SELECT count(*) AS count_as from (select id,status,auth,REPLACE(industry_subdivide, '|', ',') as industry_subdivide FROM hc_resource  ) as t where status=1 and auth = 1 and  FIND_IN_SET(?, industry_subdivide )", [$id]);
+    }
 }
