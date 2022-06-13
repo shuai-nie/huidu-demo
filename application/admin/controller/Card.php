61f3e92c0b612d605050ab37fce2368afceba4d5
Lucius <yesheng35@126.com>
2022-06-13 16:25:18 +0800

    更新时间

--- application/admin/controller/Card.php
+++ application/admin/controller/Card.php
@@ -88,20 +88,17 @@ class Card extends Base
             $cardContact = model('cardContact');
             $number = GetRandStr(16);
             $pwd = md5(md5(config('userPwd')) . $number);
-
-//            $uid = \request()->post('uid');
-//            $count = $card->where(['uid'=>$uid])->count();
-//            if( $count > 0 ) {
-//                return error_json('当前用户已创建名片', array(), 400);
-//            }
             $_post['business_tag'] = isset($_post['business_tag']) ? implode('|', $_post['business_tag']) : '';
-
             $contact = array();
-
             $state = false;
+            $userInfo = model('user_info');
+            $package = model('package');
+            $userRecharge = model('userRecharge');
             Db::startTrans();
+            $time = time();
             try {
-                $userModel->allowField(true)->save([
+                $packageInfo = $package->find(1);
+                $userModel->allowField(true)->isUpdate(false)->save([
                     'username' => $_post['username'],
                     'pwd' => $pwd,
                     'salt' => $number,
@@ -109,7 +106,28 @@ class Card extends Base
                     'head_url' => $_post['logo'],
                 ]);
                 $_post['uid'] = $userModel->id;
-                $card->allowField(true)->save($_post);
+                $userRecharge->isUpdate(false)->allowField(true)->save([
+                    'uid' => $_post['uid'],
+                    'package_id' => $packageInfo['id'],
+                    'flush' => $packageInfo['flush'],
+                    'publish' => $packageInfo['publish'],
+                    'view_demand' => $packageInfo['view_demand'],
+                    'view_provide' => $packageInfo['view_provide'],
+                    'view_provide_give' => $packageInfo['view_provide_give'],
+                    'used_flush' => 0,
+                    'used_publish' => 0,
+                    'used_view_demand' => 0,
+                    'used_view_provide' => 0,
+                    'start_time' => $time,
+                    'end_time' => $time + 86400 *30,
+                    'remarks' => '新建名片注册账号',
+                ]);
+                $userInfo->allowField(true)->isUpdate(false)->save([
+                    'uid' => $_post['uid'],
+                    'user_recharge_id' => $userRecharge->id
+                ]);
+
+                $card->allowField(true)->isUpdate(false)->save($_post);
                 foreach ($_post['contact'] as $key => $val) {
                     array_push($contact, array(
                         'card_id' => $card->id,
@@ -126,9 +144,9 @@ class Card extends Base
                 Db::rollback();;
             }
             if($state !== false){
-                return success_json();
+                return success_json('添加成功');
             }
-            return error_json();
+            return error_json('添加失败');
         }
         $str_name = config('usernameRand') . date('y') . GetRandStr(6) . date('md');
         $this->getDataDicTypeNo();
@@ -214,4 +232,18 @@ class Card extends Base
             return error_json('当前用户未创建名片', array(), 200);
         }
     }
+
+    public function delete()
+    {
+        $id = \request()->param('id');
+        if($id > 0) {
+            $state = model('card')->isUpdate(true)->save(['status'=>0], ['id'=>$id]);
+            if($state !== false ){
+                return success_json('删除名片成功');
+            }
+            return error('删除名片失败');
+        }
+
+    }
+
 }
