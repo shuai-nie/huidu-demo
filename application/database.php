78e0b8a36a7041abfa40fde470e6be35a9e8935d
Lucius <yesheng35@126.com>
2022-06-15 18:15:43 +0800

    asd

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.56.58.172',
+    'hostname'        => 'rm-3nsack8bdr611l6s1xo.mysql.rds.aliyuncs.com',
     //'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'ziyuanku',
