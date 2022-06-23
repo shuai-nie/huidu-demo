79114390bbd94cf9cc491a83871ab28084e08002
Lucius <yesheng35@126.com>
2022-06-23 17:59:04 +0800

    asd

--- application/admin/model/PackagePrice.php
+++ application/admin/model/PackagePrice.php
@@ -3,6 +3,12 @@ namespace app\admin\model;
 
 class PackagePrice extends Base
 {
+    public $type = array(
+        array('id'=>1, 'title'=>'月'),
+        array('id'=>2, 'title'=>'季'),
+        array('id'=>3, 'title'=>'年'),
+    );
+
     protected $autoWriteTimestamp = true;
     protected $createTime = 'create_time';
     protected $updateTime = 'update_time';
