c7b703b9d2de1c670f865d7a8d2cdc3232fb787a
Lucius <yesheng35@126.com>
2022-06-24 19:23:58 +0800

    asd

--- application/admin/model/Message.php
+++ application/admin/model/Message.php
@@ -3,10 +3,14 @@ namespace app\admin\model;
 
 class Message extends Base
 {
+    public $base_type = [
+        ['id' => 1, 'title' => '系统消息'],
+        ['id' => 1, 'title' => '活动消息'],
+    ];
     protected $autoWriteTimestamp = true;
     protected $createTime = 'create_time';
     protected $updateTime = 'update_time';
-    protected $insert = ['status' => 1, 'create_id', 'update_id'];
+    protected $insert = ['status' => 1, 'create_id', 'update_id','source_type'=>1];
     protected $update = ['update_id'];
 
     protected function setCreateIdAttr()
---
+++ application/admin/model/MessageLink.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\model;
+
+class MessageLink extends Base
+{
+    protected $autoWriteTimestamp = true;
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
+
+    public $link_type = [
+        ['id' => 1, 'title' => '消息封面'],
+        ['id' => 2, 'title' => '消息ICON'],
+        ['id' => 3, 'title' => '详情链接'],
+        ['id' => 4, 'title' => '联系客服'],
+    ];
+
+}
