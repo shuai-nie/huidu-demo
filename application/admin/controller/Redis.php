9cf9f88909b650fcb8ab758bbab257adf75c7198
Dave <183778738@qq.com>
2022-10-11 15:46:04 +0800

    asd

--- application/admin/controller/Redis.php
+++ application/admin/controller/Redis.php
@@ -4,32 +4,33 @@ namespace app\admin\controller;
 class Redis extends Base
 {
     public $redisData = [
-        ['title'=>'首页banner', 'key'=>'bannerList:getBannerList'],
-        ['title'=>'广告列表', 'key'=>'ad_all_list'],
-        ['title'=>'config配置', 'key'=>'configList:getConfigList'],
-        ['title'=>'合作动态', 'key'=>'CooperationList:getCooperationList'],
-        ['title'=>'首页级联菜单', 'key'=>'HomeTypeAll:getTypeAll'],
-        ['title'=>'字典缓存', 'key'=>'dataDicTypeList:*'],
-        ['title'=>'资源字典缓存', 'key'=>'listByDataTypeNos:listByDataTypeNos'],
-        ['title'=>'首页滚动资源', 'key'=>'homeResource:*'],
-        ['title'=>'首页推荐资源', 'key'=>'HomeDemandList:*'],
-        ['title'=>'区号列表', 'key'=>'CountryMobilePrefixList:getList'],
+        ['title' => '首页banner', 'key' => 'bannerList:getBannerList'],
+        ['title' => '广告列表', 'key' => 'ad_all_list'],
+        ['title' => 'config配置', 'key' => 'configList:getConfigList'],
+        ['title' => '合作动态', 'key' => 'CooperationList:getCooperationList'],
+        ['title' => '首页级联菜单', 'key' => 'HomeTypeAll:getTypeAll'],
+        ['title' => '字典缓存', 'key' => 'dataDicTypeList:*'],
+        ['title' => '资源字典缓存', 'key' => 'listByDataTypeNos:listByDataTypeNos'],
+        ['title' => '首页滚动资源', 'key' => 'homeResource:*'],
+        ['title' => '首页推荐资源', 'key' => 'HomeDemandList:*'],
+        ['title' => '区号列表', 'key' => 'CountryMobilePrefixList:getList'],
     ];
+
     /*
      * 接口： /api/removerCache
-请求方式：Post
-参数：cacheName
-缓存列表：
-首页banner：bannerList:getBannerList
-广告列表：ad_all_list
-config配置：configList:getConfigList
-合作动态：CooperationList:getCooperationList
-首页级联菜单：HomeTypeAll:getTypeAll
-字典缓存：dataDicTypeList:*
-资源字典缓存：listByDataTypeNos:listByDataTypeNos
-首页滚动资源：homeResource:*
-首页推荐资源：HomeDemandList:*
-区号列表：CountryMobilePrefixList:getList
+        请求方式：Post
+        参数：cacheName
+        缓存列表：
+        首页banner：bannerList:getBannerList
+        广告列表：ad_all_list
+        config配置：configList:getConfigList
+        合作动态：CooperationList:getCooperationList
+        首页级联菜单：HomeTypeAll:getTypeAll
+        字典缓存：dataDicTypeList:*
+        资源字典缓存：listByDataTypeNos:listByDataTypeNos
+        首页滚动资源：homeResource:*
+        首页推荐资源：HomeDemandList:*
+        区号列表：CountryMobilePrefixList:getList
      * */
 
     public function index()
