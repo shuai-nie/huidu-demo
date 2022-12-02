f34ca3da0cf8271a07203d26c43bc8f48fb9900d
Dave <183778738@qq.com>
2022-12-02 11:45:29 +0800

    asd

--- application/api/controller/Reptile.php
+++ application/api/controller/Reptile.php
@@ -275,6 +275,7 @@ class Reptile extends Controller
                 $val['detail'] = str_replace('图虫创意', '', $val['detail']);
                 $val['detail'] = str_replace('图片来源：', '', $val['detail']);
                 $val['detail'] = str_replace('</p>', '</p><br/>', $val['detail']);
+                exit();
                 if (!empty($list['value'])) {
                     $value = explode("\n", $list['value']);
                     foreach ($value as $valCon) {
