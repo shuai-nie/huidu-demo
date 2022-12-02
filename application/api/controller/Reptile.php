698283e9a1cba450400905095234d30868b16815
Dave <183778738@qq.com>
2022-12-02 14:15:19 +0800

    asd

--- application/api/controller/Reptile.php
+++ application/api/controller/Reptile.php
@@ -272,6 +272,7 @@ class Reptile extends Controller
                 }
 
                 $val['detail'] = (new ApiReptile())->CifNewsArticle($val['link']);
+                var_dump($val['detail']);exit();
                 $val['detail'] = str_replace('图虫创意', '', $val['detail']);
                 $val['detail'] = str_replace('图片来源：', '', $val['detail']);
                 $val['detail'] = str_replace('</p>', '</p><br/>', $val['detail']);
