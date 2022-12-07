5c446a0b295b267dc99ac047072097d1d7fbbf62
Dave <183778738@qq.com>
2022-12-07 18:41:01 +0800

    导航里配置

--- application/admin/controller/Navigation.php
+++ application/admin/controller/Navigation.php
@@ -9,7 +9,7 @@ class Navigation extends Base
     public function index()
     {
         if(request()->isPost()){
-            $data = NavigationBar::where([])->order("sort desc, id desc")->select();
+            $data = NavigationBar::where(['status'=>1])->order("sort desc, id desc")->select();
             return json(['code'=>0,'count'=>0,'data'=>$data], 200);
         }
         return view('', ['meta_title' => '导航栏配置']);
@@ -28,6 +28,33 @@ class Navigation extends Base
         return view('', ['link_type'=>NavigationBar::$link_type]);
     }
 
+    public function edit()
+    {
+        $id = request()->param('id');
+        $map = ['id'=>$id];
+        if(request()->isPost()){
+            $_post = request()->post();
+            $state = NavigationBar::update($_post, $map);
+            if($state != false){
+                return success_json('编辑成功');
+            }
+            return error_json('编辑失败');
+        }
+        $data = NavigationBar::where($map)->find();
+        return view('add', ['link_type' => NavigationBar::$link_type, 'data'=> $data]);
+    }
+
+    public function delete()
+    {
+        $id = request()->param('id');
+        $map = ['id'=>$id];
+        $state = NavigationBar::update(['status'=>0], $map);
+        if($state != false){
+            return success_json('编辑成功');
+        }
+        return error_json('编辑失败');
+    }
+
     public function json()
     {
         Tree::config([
@@ -39,7 +66,7 @@ class Navigation extends Base
             'step'  => 4,
         ]);
 
-        $data = NavigationBar::where([])->field('id,pid,title')->order("sort desc, id desc")->select();
+        $data = NavigationBar::where(['status'=>1])->field('id,pid,title')->order("sort desc, id desc")->select();
         if($data) {
             $data = collection($data)->toArray();
         }
